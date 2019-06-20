500c2a6b4664f39f17c397e5ce95e5685c620e63
Jeong-Hyun Boo <jeong-hyunboo@u.boisestate.edu>
2019-06-20 15:24:46 -0600

    Created and completed Subtraction class

---
+++ Subtraction.java
@@ -0,0 +1,21 @@
+/**
+ * This class represents a subtraction feature of the calculator.
+ * 
+ * @author Jeong-Hyun Boo
+ *
+ */
+
+public class Subtraction extends BinaryOperator {
+
+	@Override
+	public double evaluate(double input1, double input2) throws ArithmeticException {
+		// TODO Auto-generated method stub
+		return input1 - input2;
+	}
+	
+	@Override
+	public String betweenSymbol() {
+		// TODO Auto-generated method stub
+		return "-";
+	}
+}
