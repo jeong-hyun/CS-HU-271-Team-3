2bd4582218695e885e5fc64904ab4ee82f8217eb
JEONG-HYUN BOO <jeong-hyunboo@u.boisestate.edu>
2019-06-25 16:55:11 -0600

    Issue #17, 18 Cosine and sine classes completed.

--- Cosine.java
+++ Cosine.java
@@ -1,1 +1,18 @@
+/**
+ * This class represents a cosine feature of the calculator.
+ * 
+ * @author Jeong-Hyun Boo
+ *
+ */
+
+public class Cosine extends UnaryOperator 
+{
+	@Override
+	public double evaluate(double input1) throws ArithmeticException 
+	{
+		// TODO Auto-generated method stub
+		return Math.cos(input1);
+	}
+
+}
 
--- Sine.java
+++ Sine.java
@@ -1,1 +1,19 @@
+/**
+ * This class represents a sine feature of the calculator.
+ * 
+ * @author Jeong-Hyun Boo
+ *
+ */
+
+public class Sine extends UnaryOperator 
+{
+	@Override
+	public double evaluate(double input1) throws ArithmeticException 
+	{
+		// TODO Auto-generated method stub
+		return Math.sin(input1);
+	}
+
+}
+
 
