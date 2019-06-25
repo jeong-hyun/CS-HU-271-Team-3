1ab39ede5510c8200f38f8d2c28c703fb1be104b
Jeong-Hyun Boo <jeong-hyunboo@u.boisestate.edu>
2019-06-25 16:22:09 -0600

    Issue #6 Completed(?)
    It may not be fully complete. It may be.

--- TextUI.java
+++ TextUI.java
@@ -1,11 +1,46 @@
+import java.util.Scanner;
+
 /**
+ * Driver class
  * 
  * 
  * @author Jeong-Hyun Boo
  *
  */
+public class TextUI
 
-public class TextUI {
+{
+	private static String num;
+	private static String oprtr;
 
-	
+	public static void main(String args[]) 
+	{
+		CalculatorState state = new SimpleCalcState();
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNext()) 
+		{
+			if (scan.hasNextDouble()) 
+			{
+				num = scan.next();
+				state.pushNumber(num);
+			} else 
+			{
+				try 
+				{
+					state.pushOperator(scan.next());
+				} catch (InputOrderException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (OperationNotFoundException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		scan.close();
+	}
+
 }
