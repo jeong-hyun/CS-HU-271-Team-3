20350664b8f2da0fa24531c49fe2ae5901ddf4f6
JEONG-HYUN BOO <jeong-hyunboo@u.boisestate.edu>
2019-07-02 10:52:17 -0600

    Issue #31 Completed Tangent class

--- Tangent.java
+++ Tangent.java
@@ -1,1 +1,22 @@
+/**
+ * This class represents a tangent feature of the calculator.
+ * 
+ * @author Jeong-Hyun Boo
+ *
+ */
 
+public class Tangent extends UnaryOperator 
+{
+	@Override
+	public double evaluate(double input1) throws ArithmeticException 
+	{
+		// TODO Auto-generated method stub
+		return Math.tan(input1);
+	}
+
+	@Override
+	public String beforeSymbol() 
+	{
+		return "tan";
+	}
+}
