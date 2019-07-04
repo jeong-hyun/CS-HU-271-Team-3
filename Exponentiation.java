4a38ca11d7e981061f24d60e2e16d0e6ea4ec45c
Akira Enderle <enderle.akira@gmail.com>
2019-07-03 18:43:14 -0600

    Fixed Exponentation

--- Exponentiation.java
+++ Exponentiation.java
@@ -13,12 +13,11 @@ public class Exponentiation extends BinaryOperator {
 
 	@Override
 	public double evaluate(double input1, double input2) {
-	    return Math.pow(input1, input2);
+		return Math.pow(input1, input2);
 	}
 
 	@Override
 	public String betweenSymbol() {
 		return "^";
 	}
-
 }
