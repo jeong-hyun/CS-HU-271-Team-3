2c1aea34ee2300cbd4e42191a9aa2e5acc26ca5b
Akira Enderle <enderle.akira@gmail.com>
2019-07-05 04:29:41 -0600

    Added Akira's input on the team deliverable.

---
+++ Modulo.java
@@ -0,0 +1,25 @@
+/*
+ * Performs the modulo operation for calculator.
+ * Given two numbers, the first number will be divided by the second,
+ * and the remainder of that division is returned.
+ * 
+ * @author Akira Enderle
+ * 
+ */
+
+public class Modulo extends BinaryOperator {
+	@Override
+	public OrderOfOperations stage() {
+		return OrderOfOperations.MULTIPLICATION_AND_DIVISION;
+	}
+
+	@Override
+	public double evaluate(double input1, double input2) {
+		return input1 % input2;
+	}
+
+	@Override
+	public String betweenSymbol() {
+		return "%";
+	}
+}
